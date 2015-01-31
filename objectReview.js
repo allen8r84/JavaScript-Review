a905d8e5561c34e58ea765e12216355b1deb2981
Jon <jon@allen8r.com>
2015-01-31 12:26:44 -0700

    third problem in assignment done

--- objectReview.js
+++ objectReview.js
@@ -69,18 +69,25 @@ console.log(user);
 //create an empty object called methodCollection. 
 
   //Your code here
+  var methodCollection = {};
 
 //Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
 
   //Your code here
-
+methodCollection.alertHello = function(){
+    alert("hello");
+};
+methodCollection.logHello = function() {
+    console.log("hello");
+};
 //Now call your alertHello and logHello methods. 
 
   //Your code here
+methodCollection.alertHello();
+methodCollection.logHello();
 
 
-
-/* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
+/*#4  === NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
